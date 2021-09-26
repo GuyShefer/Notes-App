442b05a5416ce72b757b4074b4a3667c9dc8d15c
Guy <shefer.guy1@gmail.com>
2021-09-26 16:49:28 +0300

    design an app structure

---
+++ src/components/note-input/NoteInput.component.tsx
@@ -0,0 +1,31 @@
+import React, { ChangeEvent, useState } from 'react';
+
+interface INewNoteInputProps {
+    addNote(note: string): void;
+}
+
+export const NoteInput: React.FC<INewNoteInputProps> = ({ addNote }) => {
+    const [note, setNote] = useState("");
+
+    const updateNote = (e: ChangeEvent<HTMLInputElement>) => {
+        setNote(e.target.value);
+    }
+
+    const onAddNoteClick = () => {
+        addNote(note);
+        setNote("");
+    }
+
+    return (
+        <>
+            <div>
+                <input onChange={updateNote} value={note} type="text" name="note" placeholder="Note" />
+                <button onClick={onAddNoteClick}>Add Note</button>
+            </div>
+            <hr />
+            <ul>
+                <li>Some Note</li>
+            </ul>
+        </>
+    )
+}
