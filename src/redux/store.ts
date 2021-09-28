a2fccfd0e18de62728c1307b2814b075344e6e18
Guy <shefer.guy1@gmail.com>
2021-09-28 13:35:50 +0300

    add redux-persist

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,4 +1,17 @@
 import { createStore } from "redux";
+import { persistReducer, persistStore } from "redux-persist";
 import { notesReducer } from "./notesReducer";
 
-export const store = createStore(notesReducer);
+import storage from 'redux-persist/lib/storage';
+
+const persistConfig = {
+    key: 'root',
+    debug: true,
+    storage
+};
+
+const persistedReducer = persistReducer<any, any>(persistConfig, notesReducer);
+
+export const store = createStore(persistedReducer);
+
+export const persistor = persistStore(store);
